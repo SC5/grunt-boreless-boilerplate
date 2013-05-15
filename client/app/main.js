1366bf3da76283b470e04b90c794ba73f668e280
Juha Mustonen <juha.p.mustonen@gmail.com>
2013-05-15 13:56:07 +0300

    Fixed a typo in Modernizr component name

--- client/app/main.js
+++ client/app/main.js
@@ -3,11 +3,11 @@ require.config({
 
 	paths : {
 		jquery: '../components/jquery/jquery.min',
-		modernizr: '../components/modernizer/modernizr'
+		modernizr: '../components/modernizr/modernizr'
 	}
 });
 
-require(['jquery', 'modernizr'], function($, modernizer) {
+require(['jquery', 'modernizr'], function($, modernizr) {
 	// use app here
 	$('#status').html('If you can read this text, your stack should be alright.');
 }); 
