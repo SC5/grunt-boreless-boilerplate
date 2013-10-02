0db056813343a5fec1f0a055cfcd5949926e4e25
Joonas Kallio <joonas@sc5.io>
2013-10-02 14:22:31 +0300

    require.js chai configs

--- client/app/main.js
+++ client/app/main.js
@@ -1,9 +1,16 @@
+
 require.config({
 	baseUrl: 'app',
 
 	paths : {
 		jquery: '../components/jquery/jquery.min',
-		modernizr: '../components/modernizr/modernizr'
+		modernizr: '../components/modernizr/modernizr',
+		// Test libraries
+		chai: '../components/chai/chai',
+		chaiChanges: '../components/chai-changes/chai-changes',
+		chaiJQuery: '../components/chai-jquery/chai-jquery',
+		sinon: '../components/sinonjs/sinon',
+		chaiBackbone: '../components/chai-backbone/chai-backbone'
 	}
 });
 
