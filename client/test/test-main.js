6846bd5449b4cfb2bb9d1ce3f312482dff123e87
Sami Karjalainen <sami.karjalainen@sc5.io>
2013-10-09 21:05:25 +0300

    change spaces to tabs to better merge with master

--- client/test/test-main.js
+++ client/test/test-main.js
@@ -1,9 +1,9 @@
 var tests = Object.keys(window.__karma__.files).filter(function (file) {
-  return (/.spec\.js$/).test(file);
+	return (/.spec\.js$/).test(file);
 });
 
 require([].concat(tests),
-  function() {
-  // Run the tests
-  window.__karma__.start();
+	function() {
+	// Run the tests
+	window.__karma__.start();
 });
