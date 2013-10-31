4779a87070a83994927b0596dd0070c7bf3b8fe4
Sami Karjalainen <sami.karjalainen@sc5.io>
2013-10-31 10:14:39 +0200

    remove old client directory

--- client/test/test-main.js
+++
@@ -1,9 +0,0 @@
-var tests = Object.keys(window.__karma__.files).filter(function (file) {
-	return (/.spec\.js$/).test(file);
-});
-
-require([].concat(tests),
-	function() {
-	// Run the tests
-	window.__karma__.start();
-});
